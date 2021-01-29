f1298f5a5e2e15b97af563bb71703a26efc53a5f
QuentinDPT <quentin@depotter.fr>
2021-01-29 20:29:10 +0100

    Update mobile menu

--- Views/Mobile/Home/script.php
+++ Views/Mobile/Home/script.php
@@ -6,6 +6,7 @@
 
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css" integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ==" crossorigin="" />
 <link rel="stylesheet" href="/src/styles/style.css">
+<link rel="stylesheet" href="/src/styles/phone.style.css">
 
 <script type="text/javascript">
   var GUser = {id:"",name:"",avatar:"",mail:""} ;
--- src/js/phone.js
+++ src/js/phone.js
@@ -1,14 +1,27 @@
-function openInFullScreen(){
-  var elem = document.getElementsById("map");
-  function openFullscreen() {
-    if (elem.requestFullscreen) {
-      elem.requestFullscreen();
-    } else if (elem.webkitRequestFullscreen) { /* Safari */
-      elem.webkitRequestFullscreen();
-    } else if (elem.msRequestFullscreen) { /* IE11 */
-      elem.msRequestFullscreen();
+
+var XOffset = 0 ;
+
+window.addEventListener("load", function(e) {
+  document.getElementById("pinOption").onmousedown = function(e){
+    XOffset = e.layerX ;
+    document.getElementById("pinOption").style.transition = "" ;
+    document.onmousemove = function(ev){
+      document.getElementById("pinOption").classList.add("expandOption");
+      document.getElementById("pinOption").style.width = (window.innerWidth - ev.clientX + XOffset) + "px" ;
     }
   }
-}
+  document.onmouseup = function(ev){
+    console.log(ev) ;
+    document.getElementById("pinOption").style.transition = ".7s" ;
 
-document.getElementsById("map").onclick = openInFullScreen ;
+    console.log(document.getElementById("pinOption").offsetWidth) ;
+
+    if(window.innerWidth - ev.clientX < XOffset + 50)
+      document.getElementById("pinOption").classList.remove("expandOption");
+    else
+      document.getElementById("pinOption").classList.add("expandOption");
+
+    document.getElementById("pinOption").style.width = "" ;
+    document.onmousemove = null ;
+  }
+});
